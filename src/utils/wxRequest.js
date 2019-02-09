3c8fefb73d3a279da2aab415e0c15e02e04de5f1
zhaop <zhaop33@sina.com>
2019-02-09 23:37:00 +0800

    悠

--- src/utils/wxRequest.js
+++ src/utils/wxRequest.js
@@ -2,6 +2,9 @@ import wepy from 'wepy';
 import util from './util';
 import md5 from './md5';
 import tip from './tip'
+import {
+  TOKEN
+} from '@/utils/constant';
 
 const API_SECRET_KEY = 'www.mall.cycle.com'
 const TIMESTAMP = util.getCurrentTime()
@@ -9,6 +12,11 @@ const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
 
 const wxRequest = async(params = {}, url) => {
     tip.loading();
+    let contentType = params.contentType || { 'Content-Type': 'application/json' };
+    let token = wx.getStorageSync(TOKEN);
+    if(token){
+      contentType.Authorization = token;
+    }
     let data = params.query || {};
     data.sign = SIGN;
     data.time = TIMESTAMP;
@@ -16,7 +24,7 @@ const wxRequest = async(params = {}, url) => {
         url: url,
         method: params.method || 'GET',
         data: data,
-        header: { 'Content-Type': 'application/json' },
+        header: contentType
     });
     tip.loaded();
     return res;
